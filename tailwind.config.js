ef1bbfce6a2c2ae4cbd6b9b820d38ce5a52ef189
Ziwen Wang <ziwen243@outlook.com>
2023-04-01 02:37:50 +0800

    改了一点

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      './public/**/*.html',
+      './src/**/*.{js,jsx,ts,tsx,vue}',
+    ],
+    darkMode: false, 
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
