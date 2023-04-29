964cac600835cd294ce5f6a725c258b8789e9e3f
Ziwen Wang <ziwen243@outlook.com>
2023-04-30 02:45:59 +0800

    实现搜索框的event，并艰难地支持了中文输入法.

--- src/components/background.js
+++ src/components/background.js
@@ -4,9 +4,9 @@ function Background(props) {
     var blur = props.enableBlur && props.isFocus
     const css =
       "w-full h-full fixed object-cover inset-0 duration-300 z-0 ";
-    var var_css = blur ? "blur-sm scale-125" : "";
+    var varCSS = blur ? "blur-sm scale-125" : "";
     return (
-      <img src={props.src} className={css + var_css} alt="background"></img>
+      <img src={props.src} className={css + varCSS} alt="background"></img>
     );
 }
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import { React, useState } from 'react';
+
 function Search(props) {
   const css =
     "relative z-1 w-[400px] h-10 rounded-3xl top-48 left-1/2 translate-x-[-50%] text-center outline-none border-solid border-0 duration-200 hover:w-[600px] focus:w-[600px] ";
-  var var_css = props.elementBackdrop
+  var varCSS = props.elementBackdrop
     ? "bg-[rgba(255,255,255,0.7)] backdrop-blur"
     : "bg-[rgba(255,255,255,0.9)]";
-  
-  function search() {
-    var engine = props.engine;
+
+  const [query, setQuery] = useState('');
+  const [isComposing, setComposingStatus] = useState(false);
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
   }
-  
+
+  function handleInput(event) {
+    const value = event.target.value;
+    setQuery(value);
+    if (!isComposing) {
+      getSearchSuggestions(value);
+    }
+  }
+
+  function handleCompositionStart(event) {
+    setComposingStatus(true);
+  }
+
+  function handleCompositionEnd(event) {
+    setComposingStatus(false);
+    setQuery(event.target.value);
+    getSearchSuggestions(event.target.value);
+  }
+
+  function handleSearch() {
+    console.log('search:', query);
+  }
+
+  function getSearchSuggestions(value) {
+    console.log('search suggestions:', value);
+  }
+
   return (
     <input
+      id="searchBox"
       type="text"
       placeholder={"搜索"}
-      className={css + var_css}
+      className={css + varCSS}
+      onChange={handleInput}
+      onKeyDown={handleKeyDown}
+      onCompositionStart={handleCompositionStart}
+      onCompositionEnd={handleCompositionEnd}
+      value={query}
     ></input>
   );
 }
