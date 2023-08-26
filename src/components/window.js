f174c1ab3614bbfff9700559dd6da2b46a9a4985
ZiWen Wang <ziwen243@outlook.com>
2023-08-26 17:38:06 +0800

    彻底重写了Window的动画，推进了一点点设置页面

--- src/components/window.js
+++ src/components/window.js
@@ -1,28 +1,46 @@
 import React, { useEffect } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import '../style/window.css';
+import '../style/scrollbar.css';
 
-const Window = ({isShow, coverBackdorp, onClose, content}) => {
+const Window = ({ isShow, elementBackdrop, onClose, content, title}) => {
 
     const handleClose = () => {
+        document.getElementById("window").style.transitionDuration = "300ms";
         document.getElementById("window").style.opacity = "0";
         setTimeout(onClose, 300); // Wait for the fade-out animation to complete before closing
     };
 
     useEffect(() => {
         if (isShow) {
-            setTimeout(() => { document.getElementById("window").style.opacity = "1" }, 10)
+            setTimeout(() => {
+                document.getElementById("window").style.transitionDuration = "150ms";
+                document.getElementById("window").style.opacity = "1";
+            }, 10)
         }
     }, [isShow])
 
+    let windowCSS = "inset-0 flex items-center justify-center z-20 fixed transition-all duration-150 opacity-0 scale-1";
+    let windowShow = isShow ? "inline" : "hidden";
+    let windowCover = elementBackdrop ? "backdrop-blur-md backdrop-brightness-75" : "backdrop-brightness-50";
+    windowCSS = windowCSS + ' ' + windowShow + ' ' + windowCover;
+
+    let titleBarCSS = "relative h-10 lg:h-14 border-b-[1px] border-solid border-neutral-300 w-full rounded-t-lg z-30";
+    let titleBarBackdrop = elementBackdrop
+            ? "bg-[rgba(255,255,255,0.7)] backdrop-blur-lg dark:bg-[rgba(24,24,24,0.7)] text-slate-700 dark:text-slate-200"
+            : "bg-[rgba(255,255,255,1)] dark:bg-[rgba(24,24,24,1)] text-slate-700 dark:text-slate-200";
+    titleBarCSS = titleBarCSS + ' ' + titleBarBackdrop;
     return (
-        <div id="window" className={"inset-0 flex items-center justify-center z-20 backdrop-blur-md fixed transition-all duration-300 " + (isShow ? "inline" : "hidden")}>
+        <div id="window" className={windowCSS}>
             {/*窗口框体*/}
             <div className="w-[85vw] sm:w-128 lg:w-144 xl:w-168
-                h-144 lg:h-160 mh:h-128 st:h-96 2xst:h-80
-                bg-white rounded-lg dark:bg-neutral-900 dark:text-slate-200">
+                            h-144 lg:h-160 mh:h-128 st:h-96 2xst:h-80
+                            bg-white rounded-lg dark:bg-neutral-900 dark:text-slate-200">
                 {/*标题栏*/}
-                <div className="relative h-10 lg:h-14 border-b-[1px] border-solid border-neutral-300 w-full">
+                <div className={titleBarCSS}>
+                    <div className="absolute h-10 w-full lg:h-14 text-center text-xl leading-5 pt-[10px] lg:pt-[18px]">
+                        <b>{title}</b> 
+                    </div>
                     <button
                         className="absolute rounded-md
                         w-12 h-8 lg:w-16 lg:h-10 mt-1 mr-1 lg:mt-2 lg:mr-2 pl-3.5 lg:pl-5 right-0
@@ -34,8 +52,9 @@ const Window = ({isShow, coverBackdorp, onClose, content}) => {
                     </button>
                 </div>
                 {/*内容*/}
-                <div className="overflow-y-scroll overflow-x-hidden text-justify pl-4 pr-4 pt-4 h-134 lg:h-144
-                    mh:h-118 mh:lg:h-114 st:h-86 st:lg:h-82 2xst:h-70 2xst:lg:h-64 w-full break-all">
+                <div className="overflow-y-scroll overflow-x-hidden pl-4 pr-4 pt-12 lg:pt-16 mt-[-40px] lg:mt-[-56px]
+                                h-144 lg:h-160 mh:h-128 st:h-96 2xst:h-80
+                                w-full noScrollbar">
                     {content}
                 </div>
             </div>
