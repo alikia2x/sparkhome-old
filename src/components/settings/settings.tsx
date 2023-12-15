ac6da4bb5f07072d418035afd9f37f5f3ec6ee8c
ZiWen Wang <ziwen243@outlook.com>
2023-12-15 20:36:41 +0800

    [imp]Windows和Selector样式改进

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -1,10 +1,7 @@
 import SettingsSwitchRow from "./settingsSwitchRow";
 import SettingsGroup from "./settingsGroup";
-import { useTranslation } from "react-i18next";
-import React from "react";
 
 export default function Settings() {
-    const { t } = useTranslation();
     return (
         <div className="pt-4">
             <SettingsGroup>
