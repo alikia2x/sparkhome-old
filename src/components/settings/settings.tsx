e3fc378696b935da388ce324431a711d925ff386
ZiWen Wang <ziwen243@outlook.com>
2023-12-16 20:51:37 +0800

    [imp]Selector的CSS逻辑改进

--- src/components/settings/settings.tsx
+++ src/components/settings/settings.tsx
@@ -4,10 +4,12 @@ import SettingsGroup from "./settingsGroup";
 export default function Settings() {
     return (
         <div className="pt-4">
-            <SettingsGroup>
+            <SettingsGroup title="appearance">
                 <SettingsSwitchRow settingKey="bgBlur" />
-                <SettingsSwitchRow settingKey="focusWhenLaunch"/>
                 <SettingsSwitchRow settingKey="elementBackdrop"/>
+            </SettingsGroup>
+            <SettingsGroup title="behavior">
+                <SettingsSwitchRow settingKey="focusWhenLaunch"/>
             </SettingsGroup>
         </div>
     );
